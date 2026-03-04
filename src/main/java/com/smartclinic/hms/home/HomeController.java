48129d3751eeea6c59cbf6c05861f5413fdfa769
xodh-house <rkdxodh41@gmail.com>
2026-03-05 00:03:08 +0900

    홈페이지수정중

--- src/main/java/com/smartclinic/hms/home/HomeController.java
+++ src/main/java/com/smartclinic/hms/home/HomeController.java
@@ -1,14 +1,15 @@
 package com.smartclinic.hms.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("message", "Smart Clinic에 오신 것을 환영합니다.");
         return "home/index";
     }
-
 }
