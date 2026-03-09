5ba9bd2d0b7b8ced36aeba7bb70baa413c487408
yujicoding <choyj358@gmail.com>
2026-03-09 15:29:36 +0900

    staff 접수 목록 보기

--- src/main/java/com/smartclinic/hms/staff/reception/ReceptionService.java
+++ src/main/java/com/smartclinic/hms/staff/reception/ReceptionService.java
@@ -1,5 +1,13 @@
 package com.smartclinic.hms.staff.reception;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class ReceptionService {
+
+    // 1. 접수 목록보기 (GET)
 
 }
