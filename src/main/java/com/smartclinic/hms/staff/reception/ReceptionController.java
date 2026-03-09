3459eb7e5cf6cfad43f6269aa86ac77bc39c6b9b
yujicoding <choyj358@gmail.com>
2026-03-09 16:55:18 +0900

    staff 접수처리 PostMapping까지

--- src/main/java/com/smartclinic/hms/staff/reception/ReceptionController.java
+++ src/main/java/com/smartclinic/hms/staff/reception/ReceptionController.java
@@ -3,10 +3,21 @@ package com.smartclinic.hms.staff.reception;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.smartclinic.hms.staff.reception.dto.ReceptionUpdateRequest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/staff/reception")
+@RequiredArgsConstructor
 public class ReceptionController {
+
+    private final ReceptionService receptionService;
 
     // 접수 목록
     @GetMapping("/list")
@@ -14,9 +25,22 @@ public class ReceptionController {
         return "staff/reception-list";
     }
 
-    // 접수 처리
+    // 접수 처리 화면보기기
     @GetMapping("/detail")
     public String detail() {
         return "staff/reception-detail";
     }
+
+    // 접수 처리 하기
+    @PostMapping("/receive")
+    public String receive(ReceptionUpdateRequest request,
+            RedirectAttributes redirectAttributes) {
+
+        receptionService.receive(request);
+
+        redirectAttributes.addFlashAttribute("message", "접수가 완료되었습니다.");
+        return "redirect:/staff/reception/list";
+
+    }
+
 }
--- src/main/java/com/smartclinic/hms/staff/reception/ReceptionService.java
+++ src/main/java/com/smartclinic/hms/staff/reception/ReceptionService.java
@@ -2,11 +2,17 @@ package com.smartclinic.hms.staff.reception;
 
 import org.springframework.stereotype.Service;
 
+import com.smartclinic.hms.staff.reception.dto.ReceptionUpdateRequest;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class ReceptionService {
+    public void receive(ReceptionUpdateRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'receive'");
+    }
 
     // 1. 접수 목록보기 (GET)
 
